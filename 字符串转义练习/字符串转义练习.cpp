2778d5e2bc5ec75fb3bd74b20b9851791efe28fe
zzdhhh <https://github.com/zhenda-hub/MyFirstGame.git>
2021-12-14 16:40:27 +0800

    switch练习

--- 字符串转义练习/字符串转义练习.cpp
+++ 字符串转义练习/字符串转义练习.cpp
@@ -11,6 +11,15 @@ int main()
     string s1 = R"(E:\ME\2. test\网上资源\mfc学生管理系统)";//方法2 R"()"
     cout << s << endl;
     cout << s1 << endl;
+
+    char b = 'w';
+    cout << b * 2 << endl;//字符计算， 转化为ascii再计算
+    cout << b + 2 << endl;//字符计算， 转化为ascii再计算
+    char c = 'work';
+    cout << c * 2 << endl;//字符计算， 转化为ascii再计算
+    cout << c + 2 << endl;//字符计算， 转化为ascii再计算
+    cout << 'k' + 2 << endl;//字符计算， 转化为ascii再计算
+
     return 0;
 }
 
